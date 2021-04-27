95e5ddb95bdcdde2f9beb8b97dd4326f38bac778
unknown <shahrukh1494@gmail.com>
2021-04-27 17:52:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useGlobalContext } from "./components/context";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
+function App() {
+  const { loading } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
